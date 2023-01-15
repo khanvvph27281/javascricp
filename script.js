6b90e869e4ebf49c4b5f777441cbc10aefc30fa4
khanvv <khanvu2003@gmail.com>
2023-01-15 13:34:48 +0700

    javascrip1

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+const restauant = {
+    name: 'classico Italiano',
+    location: 'khan 23 ,hai duong ,viet nam',
+    categories: ['vietnam', 'campuchia', 'thailan', 'lao'],
+    starterMenu: ['Focaccia', 'Brushetta', 'Garlic Bread', 'Caprese'],
+    mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+};
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
